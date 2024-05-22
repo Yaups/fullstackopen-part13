b3aaa2ae2c4a69f150802a7341623e89988769a4
Yaups <tpallison.ta@gmail.com>
2024-05-22 02:23:25 +0100

    Exercise 13.18 completed

--- models/blog.js
+++ models/blog.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize')
 const { sequelize } = require('../util/db')
 
+const date = new Date()
+
 class Blog extends Model {}
 Blog.init(
   {
@@ -9,8 +11,12 @@ Blog.init(
     url: { type: DataTypes.TEXT, allowNull: false },
     title: { type: DataTypes.TEXT, allowNull: false },
     likes: { type: DataTypes.INTEGER, defaultValue: 0 },
+    year: {
+      type: DataTypes.INTEGER,
+      validate: { min: 1991, max: date.getFullYear() },
+    },
   },
-  { sequelize, underscored: true, timestamps: false, modelName: 'blog' }
+  { sequelize, underscored: true, timestamps: true, modelName: 'blog' }
 )
 
 module.exports = Blog
